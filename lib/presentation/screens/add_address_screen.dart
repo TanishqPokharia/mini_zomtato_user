b510adbee3bd3076f1aaf31c48bcfe53358a25e2
TanishqPokharia <tppokharia@gmail.com>
2025-06-11 12:31:03 +0530

    fix: fixed scrolling in add_address_screen

--- lib/presentation/screens/add_address_screen.dart
+++ lib/presentation/screens/add_address_screen.dart
@@ -24,161 +24,163 @@ class _AddAddressScreenState extends State<AddAddressScreen> {
           appBar: AppBar(title: Text("Add Address")),
           body: Padding(
             padding: EdgeInsets.all(context.rs(20)),
-            child: Column(
-              spacing: 10,
-              children: [
-                TextFormField(
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a label';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    values['label'] = value ?? '';
-                  },
-                  decoration: InputDecoration(labelText: 'Label'),
-                ),
-                TextFormField(
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a state';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    values['state'] = value ?? '';
-                  },
-                  decoration: InputDecoration(labelText: 'State'),
-                ),
-                TextFormField(
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a city';
-                    }
-                    return null;
-                  },
+            child: SingleChildScrollView(
+              child: Column(
+                spacing: 10,
+                children: [
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a label';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      values['label'] = value ?? '';
+                    },
+                    decoration: InputDecoration(labelText: 'Label'),
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a state';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      values['state'] = value ?? '';
+                    },
+                    decoration: InputDecoration(labelText: 'State'),
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a city';
+                      }
+                      return null;
+                    },
 
-                  onSaved: (value) {
-                    values['city'] = value ?? '';
-                  },
+                    onSaved: (value) {
+                      values['city'] = value ?? '';
+                    },
 
-                  decoration: InputDecoration(labelText: 'City'),
-                ),
-                TextFormField(
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter address line 1';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    values['addressLine1'] = value ?? '';
-                  },
+                    decoration: InputDecoration(labelText: 'City'),
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter address line 1';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      values['addressLine1'] = value ?? '';
+                    },
 
-                  decoration: InputDecoration(labelText: 'Address Line 1'),
-                ),
-                TextFormField(
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter address line 2';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    values['addressLine2'] = value ?? '';
-                  },
-                  decoration: InputDecoration(labelText: 'Address Line 2'),
-                ),
-                TextFormField(
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a pincode';
-                    }
-                    if (int.tryParse(value) == null) {
-                      return 'Please enter a valid pincode';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    values['pincode'] = value ?? '';
-                  },
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(labelText: 'Pincode'),
-                ),
-                BlocConsumer<AddressBloc, AddressState>(
-                  listener: (context, state) {
-                    if (state is AddressSuccess) {
-                      Navigator.pop(context);
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text(
-                            state.message ?? 'Address added successfully',
+                    decoration: InputDecoration(labelText: 'Address Line 1'),
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter address line 2';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      values['addressLine2'] = value ?? '';
+                    },
+                    decoration: InputDecoration(labelText: 'Address Line 2'),
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a pincode';
+                      }
+                      if (int.tryParse(value) == null) {
+                        return 'Please enter a valid pincode';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      values['pincode'] = value ?? '';
+                    },
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(labelText: 'Pincode'),
+                  ),
+                  BlocConsumer<AddressBloc, AddressState>(
+                    listener: (context, state) {
+                      if (state is AddressSuccess) {
+                        Navigator.pop(context);
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(
+                              state.message ?? 'Address added successfully',
+                            ),
                           ),
-                        ),
-                      );
-                    }
-                  },
-                  builder: (context, state) {
-                    if (state is AddressLoading) {
+                        );
+                      }
+                    },
+                    builder: (context, state) {
+                      if (state is AddressLoading) {
+                        return Container(
+                          width: double.infinity,
+                          margin: EdgeInsets.only(top: context.rs(40)),
+                          height: context.rs(70),
+                          decoration: BoxDecoration(
+                            color: context.colorScheme.primary,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: SizedBox(
+                              height: context.rs(30),
+                              width: context.rs(30),
+                              child: CircularProgressIndicator(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        );
+                      }
                       return Container(
                         width: double.infinity,
-                        margin: EdgeInsets.only(top: context.rs(40)),
                         height: context.rs(70),
-                        decoration: BoxDecoration(
-                          color: context.colorScheme.primary,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Center(
-                          child: SizedBox(
-                            height: context.rs(30),
-                            width: context.rs(30),
-                            child: CircularProgressIndicator(
+                        margin: EdgeInsets.only(top: context.rs(40)),
+                        child: FilledButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              _formKey.currentState!.save();
+                              // Handle form submission
+                              context.read<AddressBloc>().add(
+                                AddressAddEvent(
+                                  values['label'] ?? '',
+                                  values['state'] ?? '',
+                                  values['city'] ?? '',
+                                  values['addressLine1'] ?? '',
+                                  values['addressLine2'] ?? '',
+                                  int.parse(values['pincode'] ?? ''),
+                                ),
+                              );
+                            }
+                          },
+                          style: ButtonStyle(
+                            shape: WidgetStatePropertyAll(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                          child: Text(
+                            "Save",
+                            style: context.textTheme.titleLarge!.copyWith(
                               color: Colors.white,
                             ),
                           ),
                         ),
                       );
-                    }
-                    return Container(
-                      width: double.infinity,
-                      height: context.rs(70),
-                      margin: EdgeInsets.only(top: context.rs(40)),
-                      child: FilledButton(
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            _formKey.currentState!.save();
-                            // Handle form submission
-                            context.read<AddressBloc>().add(
-                              AddressAddEvent(
-                                values['label'] ?? '',
-                                values['state'] ?? '',
-                                values['city'] ?? '',
-                                values['addressLine1'] ?? '',
-                                values['addressLine2'] ?? '',
-                                int.parse(values['pincode'] ?? ''),
-                              ),
-                            );
-                          }
-                        },
-                        style: ButtonStyle(
-                          shape: WidgetStatePropertyAll(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                        ),
-                        child: Text(
-                          "Save",
-                          style: context.textTheme.titleLarge!.copyWith(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ],
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
